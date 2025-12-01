0c487d28e3aacdfb229e40227bbce7be6ed954b0
Aurel Wünsch <aurel.wuensch@prognos.com>
2025-12-01 18:39:00 +0100

    added stub for persistent Data Class

---
+++ PersistentData/BuildingRecording.cs
@@ -0,0 +1,16 @@
+﻿using IndoorCO2MapAppV2.CO2Monitors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndoorCO2MapAppV2.PersistentData
+{
+    internal class BuildingRecording
+    {
+        long nwrid;
+        string nwrtype;
+        long recordingStart;
+        List<CO2Reading> measurementData;
+        Dictionary<string, string> additionalDataByParameter;
+    }
+}
