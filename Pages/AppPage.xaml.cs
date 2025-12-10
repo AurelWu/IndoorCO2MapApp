bc0b90b23d4410a560ac8f9834e05a57ba77fcd4
Aurel Wünsch <aurel.wuensch@prognos.com>
2025-12-10 13:25:39 +0100

    Top Status Bar now changes to a slim text if GPS & BLE are enabled and permissions granted

--- Pages/AppPage.xaml.cs
+++ Pages/AppPage.xaml.cs
@@ -1,51 +1,63 @@
 using IndoorCO2MapAppV2.Bluetooth;
-using IndoorCO2MapAppV2.ExtensionMethods;
 using IndoorCO2MapAppV2.Spatial;
+using IndoorCO2MapAppV2.ViewModels;
+using IndoorCO2MapAppV2.ExtensionMethods;
+using Microsoft.Maui.Controls;
+using System;
+using System.Threading.Tasks;
 
 namespace IndoorCO2MapAppV2.Pages
 {
     public partial class AppPage : ContentPage
     {
+        // References to bars in the ControlTemplate
+        private View? LargeBar;
+        private View? SmallBar;
 
-        //private readonly IBluetoothHelper bluetoothHelper;
-
-        // References to template controls
-        private ImageButton ButtonBluetoothStatus = null!; //set in OnApplyTemplate
-        private ImageButton ButtonBluetoothPermissions = null!;
-        private ImageButton ButtonGPSStatus = null!;
-        private ImageButton ButtonGPSPermission = null!;
         public AppPage()
         {
             InitializeComponent();
-            //bluetoothHelper = BluetoothPlatformProvider.CreateOrUse();
+
+            // Subscribe to StatusViewModel changes
+            StatusViewModel.Instance.PropertyChanged += StatusViewModel_PropertyChanged;
         }
 
         protected override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
 
-            // Get references from ControlTemplate
-            ButtonBluetoothStatus =
-                this.FindByName<ImageButton>("ButtonBluetoothStatus");
+            // Find template elements
+            LargeBar = this.GetTemplateChild("LargeBar") as View;
+            SmallBar = this.GetTemplateChild("SmallBar") as View;
 
-            ButtonBluetoothPermissions =
-                this.FindByName<ImageButton>("ButtonBluetoothPermissions");
-
-            ButtonGPSStatus =
-                this.FindByName<ImageButton>("ButtonGPSStatus");
-
-            ButtonGPSPermission =
-                this.FindByName<ImageButton>("ButtonGPSPermission");
-
+            // Initial update
+            UpdateBars();
         }
 
+        private void StatusViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(StatusViewModel.AllReady))
+            {
+                UpdateBars();
+            }
+        }
 
+        private void UpdateBars()
+        {
+            if (LargeBar == null || SmallBar == null)
+                return;
+
+            bool allReady = StatusViewModel.Instance.AllReady;
+
+            LargeBar.IsVisible = !allReady;
+            SmallBar.IsVisible = allReady;
+        }
+
+        // --- Navigation helper ---
         private void OnNavigateClicked(object sender, EventArgs e)
         {
             if (sender is Button button && button.CommandParameter is string route)
-            {
                 NavigateAsync(route).SafeFireAndForget();
-            }
         }
 
         public static async Task NavigateAsync(string route)
@@ -53,26 +65,33 @@ namespace IndoorCO2MapAppV2.Pages
             await Shell.Current.GoToAsync(route);
         }
 
-
+        // --- GPS/Bluetooth click handlers ---
         private void OnRequestGPSEnableDialog(object sender, EventArgs e)
         {
-            //LocationServicePlatformProvider.CreateOrUse() => should probably be changed to just get instance of the singleton instead which then internally creates it if it doesnt already...)
-            LocationServicePlatformProvider.CreateOrUse().ShowEnableGpsDialogAsync().SafeFireAndForget();
+            LocationServicePlatformProvider.CreateOrUse()
+                .ShowEnableGpsDialogAsync()
+                .SafeFireAndForget();
         }
 
         private void OnRequestGPSPermissionDialog(object sender, EventArgs e)
         {
-            LocationServicePlatformProvider.CreateOrUse().RequestLocationPermissionAsync().SafeFireAndForget();
+            LocationServicePlatformProvider.CreateOrUse()
+                .RequestLocationPermissionAsync()
+                .SafeFireAndForget();
         }
 
         private void OnRequestBluetoothEnableDialog(object sender, EventArgs e)
         {
-            BluetoothPlatformProvider.CreateOrUse().RequestBluetoothEnableAsync().SafeFireAndForget();
+            BluetoothPlatformProvider.CreateOrUse()
+                .RequestBluetoothEnableAsync()
+                .SafeFireAndForget();
         }
 
         private void OnRequestBluetoothPermissionsDialog(object sender, EventArgs e)
         {
-            BluetoothPlatformProvider.CreateOrUse().RequestPermissionsAsync().SafeFireAndForget();
-        }        
+            BluetoothPlatformProvider.CreateOrUse()
+                .RequestPermissionsAsync()
+                .SafeFireAndForget();
+        }
     }
 }
--- ViewModels/StatusViewModel.cs
+++ ViewModels/StatusViewModel.cs
@@ -10,10 +10,11 @@ namespace IndoorCO2MapAppV2.ViewModels
 {
     public partial class StatusViewModel : INotifyPropertyChanged
     {
-        // Singleton instance for app-wide usage
+        // Singleton instance
         private static StatusViewModel? _instance;
         public static StatusViewModel Instance => _instance ??= new StatusViewModel();
 
+        // --- Bluetooth ---
         private bool _isBluetoothOn;
         public bool IsBluetoothOn
         {
@@ -28,6 +29,7 @@ namespace IndoorCO2MapAppV2.ViewModels
             set => SetProperty(ref _bluetoothPermissionGranted, value);
         }
 
+        // --- GPS ---
         private bool _isGpsOn;
         public bool IsGpsOn
         {
@@ -42,7 +44,10 @@ namespace IndoorCO2MapAppV2.ViewModels
             set => SetProperty(ref _gpsPermissionGranted, value);
         }
 
+        // --- is GPS and BT Ready ---
+        public bool AllReady => IsGpsOn && GpsPermissionGranted && IsBluetoothOn && BluetoothPermissionGranted;
 
+        // --- PropertyChanged implementation ---
         public event PropertyChangedEventHandler? PropertyChanged;
 
         protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string? propertyName = null)
@@ -52,16 +57,21 @@ namespace IndoorCO2MapAppV2.ViewModels
 
             backingStore = value;
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+            // Notify AllReady if any component changed
+            if (propertyName != nameof(AllReady))
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AllReady)));
+
             return true;
         }
 
+        // --- Timer for periodic updates ---
         private readonly System.Timers.Timer _statusTimer;
 
         // Private constructor for singleton
         private StatusViewModel()
         {
-            // Example: update every second
-            _statusTimer = new System.Timers.Timer(1000);
+            _statusTimer = new System.Timers.Timer(1000); // every second
             _statusTimer.Elapsed += StatusTimer_Elapsed;
             _statusTimer.AutoReset = true;
             _statusTimer.Start();
@@ -69,20 +79,18 @@ namespace IndoorCO2MapAppV2.ViewModels
 
         private void StatusTimer_Elapsed(object? sender, ElapsedEventArgs e)
         {
-            // This runs on a background thread, so use MainThread for UI updates
-            MainThread.BeginInvokeOnMainThread(() =>
-            {
-                UpdateStatus();
-            });
+            // Ensure UI updates on main thread
+            MainThread.BeginInvokeOnMainThread(UpdateStatus);
         }
 
         private void UpdateStatus()
         {
-            // Use singleton BLE helper
+            // --- Bluetooth status ---
             var bluetoothHelper = BluetoothPlatformProvider.CreateOrUse();
             IsBluetoothOn = bluetoothHelper.CheckIfBTEnabled();
             BluetoothPermissionGranted = bluetoothHelper.CheckPermissions();
 
+            // --- GPS status ---
             var locationHelper = LocationServicePlatformProvider.CreateOrUse();
             UpdateGpsStatusAsync(locationHelper).SafeFireAndForget();
         }
@@ -94,7 +102,6 @@ namespace IndoorCO2MapAppV2.ViewModels
                 bool gpsOn = await locationHelper.IsGpsEnabledAsync();
                 bool permGranted = await locationHelper.HasLocationPermissionAsync();
 
-                // Update UI on main thread
                 MainThread.BeginInvokeOnMainThread(() =>
                 {
                     IsGpsOn = gpsOn;
@@ -103,17 +110,13 @@ namespace IndoorCO2MapAppV2.ViewModels
             }
             catch (Exception ex)
             {
-                // fallback in case something fails (maybe we should just do nothing instead, but lets try)
                 MainThread.BeginInvokeOnMainThread(() =>
                 {
                     IsGpsOn = false;
                     GpsPermissionGranted = false;
                 });
-
                 Console.WriteLine($"UpdateGpsStatusAsync failed: {ex.Message}");
             }
         }
-
-
     }
 }
