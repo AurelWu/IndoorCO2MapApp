be139091f6d686ef51eb90678f8db40bea511e93
Aurel Wünsch <aurel.wuensch@prognos.com>
2026-03-21 10:34:08 +0100

    merge from private branch with lots of updates

--- Pages/AppPage.xaml.cs
+++ Pages/AppPage.xaml.cs
@@ -13,6 +13,9 @@ namespace IndoorCO2MapAppV2.Pages
         // References to bars in the ControlTemplate
         private View? LargeBar;
         private View? SmallBar;
+        private View? TickerContainer;
+        private Label? TickerLabel;
+        private CancellationTokenSource? _tickerCts;
 
         public AppPage()
         {
@@ -29,16 +32,88 @@ namespace IndoorCO2MapAppV2.Pages
             // Find template elements
             LargeBar = this.GetTemplateChild("LargeBar") as View;
             SmallBar = this.GetTemplateChild("SmallBar") as View;
+            TickerContainer = this.GetTemplateChild("StatusTicker") as View;
+            TickerLabel = this.GetTemplateChild("TickerLabel") as Label;
 
             // Initial update
             UpdateBars();
+            UpdateTicker();
         }
 
         private void StatusViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             if (e.PropertyName == nameof(StatusViewModel.AllReady))
-            {
                 UpdateBars();
+
+            if (e.PropertyName == nameof(StatusViewModel.HasStatusMessage))
+                MainThread.BeginInvokeOnMainThread(UpdateTicker);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            UpdateTicker();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            StopTicker();
+        }
+
+        private void UpdateTicker()
+        {
+            StopTicker();
+            if (StatusViewModel.Instance.HasStatusMessage)
+            {
+                _tickerCts = new CancellationTokenSource();
+                _ = RunTickerAsync(_tickerCts.Token);
+            }
+        }
+
+        private void StopTicker()
+        {
+            _tickerCts?.Cancel();
+            _tickerCts = null;
+            if (TickerLabel != null)
+                Microsoft.Maui.Controls.ViewExtensions.CancelAnimations(TickerLabel);
+        }
+
+        private async Task RunTickerAsync(CancellationToken ct)
+        {
+            if (TickerLabel == null || TickerContainer == null) return;
+            await Task.Delay(400, ct).ConfigureAwait(false); // let layout settle
+
+            // Measure the label at unconstrained width to get natural text width,
+            // then set AbsoluteLayout bounds so the label renders its full text
+            // (Grid constrains child WidthRequest but AbsoluteLayout does not)
+            double naturalWidth = 200;
+            await MainThread.InvokeOnMainThreadAsync(() =>
+            {
+                var size = TickerLabel.Measure(double.PositiveInfinity, double.PositiveInfinity);
+                if (size.Width > 0) naturalWidth = size.Width;
+                AbsoluteLayout.SetLayoutBounds(TickerLabel, new Rect(0, 0, naturalWidth, 28));
+            });
+
+            while (!ct.IsCancellationRequested)
+            {
+                await MainThread.InvokeOnMainThreadAsync(() =>
+                {
+                    double containerWidth = TickerContainer.Width > 0 ? TickerContainer.Width : 400;
+                    TickerLabel.TranslationX = containerWidth;
+                });
+
+                if (ct.IsCancellationRequested) break;
+
+                await MainThread.InvokeOnMainThreadAsync(async () =>
+                {
+                    double containerWidth = TickerContainer.Width > 0 ? TickerContainer.Width : 400;
+                    uint duration = (uint)((containerWidth + naturalWidth) * 12); // ~83 px/s
+                    await TickerLabel.TranslateTo(-naturalWidth, 0, duration, Easing.Linear);
+                });
+
+                if (ct.IsCancellationRequested) break;
+                await Task.Delay(300, ct).ConfigureAwait(false);
             }
         }
 
